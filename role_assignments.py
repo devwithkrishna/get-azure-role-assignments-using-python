8cace7d6fc0e908afd4e8644e61483ad4da0b39e
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-06-21 17:43:41 +0530

    push

--- role_assignments.py
+++ role_assignments.py
@@ -1,10 +1,10 @@
 import argparse
 import os
 import json
+import subprocess
 from dotenv import load_dotenv
 from azure.mgmt.authorization import AuthorizationManagementClient
 from azure.identity import DefaultAzureCredential
-from azure.mgmt.resource import ResourceManagementClient
 
 def list_all_role_assignments_on_subscription_level(subscription_id: str):
     """
@@ -12,21 +12,18 @@ def list_all_role_assignments_on_subscription_level(subscription_id: str):
     :return: all role assignemnts in the subscription level
     """
     load_dotenv()
-    scope=f'subscriptions/{subscription_id}/'.format(subscription_id=subscription_id)
+    scope=f'{subscription_id}'.format(subscription_id=subscription_id)
     print(f'Current Scope is : {scope}')
     # Create client
     # # For other authentication approaches, please see: https://pypi.org/project/azure-identity/
-    resource_client = ResourceManagementClient(
-        credential=DefaultAzureCredential(),
-        subscription_id=subscription_id
-    )
+
     authorization_client = AuthorizationManagementClient(
         credential=DefaultAzureCredential(),
         subscription_id=subscription_id,
         api_version="2018-01-01-preview"
     )
     # Retrieve role assignment
-    get_all_role_assignment = authorization_client.role_assignments.list(filter='atScope()')
+    get_all_role_assignment = authorization_client.role_assignments.list_for_subscription()
     print(f'Role assignments retrieved.... ')
     get_all_role_assignment_list = []
     for role_assignment in get_all_role_assignment:
@@ -35,8 +32,10 @@ def list_all_role_assignments_on_subscription_level(subscription_id: str):
         role_assignment_dict['id'] = role_assignment.id
         role_assignment_dict['name'] = role_assignment.name
         role_assignment_dict['type'] = role_assignment.type
+        role_assignment_dict['principal_id'] = role_assignment.principal_id
         get_all_role_assignment_list.append(role_assignment_dict)
     return get_all_role_assignment_list
+
 
 def main():
     """ To test the code"""
@@ -45,8 +44,9 @@ def main():
 
     args = parser.parse_args()
     subscription_id = args.subscription_id
-    print("Proccess to retrieve azure role assignments started......")
-    get_all_role_assignment_list = list_all_role_assignments_on_subscription_level(subscription_id=subscription_id)
+    print("Proccess to retrieve azure role assignments initiated......")
+    # get_all_role_assignment_list = list_all_role_assignments_on_subscription_level(subscription_id=subscription_id)
+    get_all_role_assignment_list = list_role_assgnmnt_azcli(subscription_id=subscription_id)
     file_name = f'role_assignments_{subscription_id}.json'
     # Assuming role_assignment_dict is already populated
     with open(file_name, 'w') as json_file:
